billjamesevans/ai-native-luanti

commit 65b7b6d1a5fe120e5e3982c5befac82ff36aceec

Don't ignore server disconnects in client code
If the server stops talking to us without saying bye we
should actually end the in-game session with an error message.

src/client/client.cpp
@@ -786,16 +786,18 @@ void Client::peerAdded(con::Peer *peer)
 	infostream << "Client::peerAdded(): peer->id="
 			<< peer->id << std::endl;
 }
+
 void Client::deletingPeer(con::Peer *peer, bool timeout)
 {
 	infostream << "Client::deletingPeer(): "
 			"Server Peer is getting deleted "
 			<< "(timeout=" << timeout << ")" << std::endl;
 
-	if (timeout) {
-		m_access_denied = true;
+	m_access_denied = true;
+	if (timeout)
 		m_access_denied_reason = gettext("Connection timed out.");
-	}
+	else
+		m_access_denied_reason = gettext("Connection aborted (protocol error?).");
 }
 
 /*

commit f83ed4a6bee3e09790d3251d40a88f1e1a448f5d

Make sure real disconnect reason isn't overwritten
bug introduced in 65b7b6d1a5fe120e5e3982c5befac82ff36aceec

src/client/client.cpp
@@ -803,7 +803,7 @@ void Client::deletingPeer(con::Peer *peer, bool timeout)
 	m_access_denied = true;
 	if (timeout)
 		m_access_denied_reason = gettext("Connection timed out.");
-	else
+	else if (m_access_denied_reason.empty())
 		m_access_denied_reason = gettext("Connection aborted (protocol error?).");
 }
 