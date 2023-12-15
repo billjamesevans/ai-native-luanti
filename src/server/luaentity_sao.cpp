billjamesevans/ai-native-luanti

commit d3ed1a911949d490b7f30421c016ac891992de96

Warning: inform about entity name when bug detected about attachement (#13354)

src/server/luaentity_sao.cpp
@@ -145,7 +145,7 @@ void LuaEntitySAO::step(float dtime, bool send_recommended)
 	// If attached, check that our parent is still there. If it isn't, detach.
 	if (m_attachment_parent_id && !isAttached()) {
 		// This is handled when objects are removed from the map
-		warningstream << "LuaEntitySAO::step() id=" << m_id <<
+		warningstream << "LuaEntitySAO::step() " << m_init_name << " at: " << PP(m_last_sent_position) << ", id=" << m_id <<
 			" is attached to nonexistent parent. This is a bug." << std::endl;
 		clearParentAttachment();
 		sendPosition(false, true);

commit bb97f25b3c3d2bace217e264bba8a53f2d8ec2e8

Remove usage of removed "PP" macro
This fixes a compilation error introduced by d3ed1a9.

src/server/luaentity_sao.cpp
@@ -145,7 +145,7 @@ void LuaEntitySAO::step(float dtime, bool send_recommended)
 	// If attached, check that our parent is still there. If it isn't, detach.
 	if (m_attachment_parent_id && !isAttached()) {
 		// This is handled when objects are removed from the map
-		warningstream << "LuaEntitySAO::step() " << m_init_name << " at: " << PP(m_last_sent_position) << ", id=" << m_id <<
+		warningstream << "LuaEntitySAO::step() " << m_init_name << " at " << m_last_sent_position << ", id=" << m_id <<
 			" is attached to nonexistent parent. This is a bug." << std::endl;
 		clearParentAttachment();
 		sendPosition(false, true);