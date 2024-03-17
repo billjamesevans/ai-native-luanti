billjamesevans/ai-native-luanti

commit 0671a392be92bc06ab1f33bc47d3bddd6fa72c20

Allow toggling touchscreen mode at runtime (#14075)
Signed-off-by: David Heidelberg <david@ixit.cz>
Co-authored-by: Gregor Parzefall <gregor.parzefall@posteo.de>

src/client/inputhandler.cpp
@@ -102,11 +102,9 @@ bool MyEventReceiver::OnEvent(const SEvent &event)
 		React to nothing here if a menu is active
 	*/
 	if (isMenuActive()) {
-#ifdef HAVE_TOUCHSCREENGUI
 		if (m_touchscreengui) {
 			m_touchscreengui->setVisible(false);
 		}
-#endif
 		return g_menumgr.preprocessEvent(event);
 	}
 
@@ -130,12 +128,10 @@ bool MyEventReceiver::OnEvent(const SEvent &event)
 			return true;
 		}
 
-#ifdef HAVE_TOUCHSCREENGUI
 	} else if (m_touchscreengui && event.EventType == irr::EET_TOUCH_INPUT_EVENT) {
 		// In case of touchscreengui, we have to handle different events
 		m_touchscreengui->translateEvent(event);
 		return true;
-#endif
 
 	} else if (event.EventType == irr::EET_JOYSTICK_INPUT_EVENT) {
 		// joystick may be nullptr if game is launched with '--random-input' parameter

.luacheckrc
@@ -71,7 +71,6 @@ files["builtin/mainmenu"] = {
 
 	read_globals = {
 		"PLATFORM",
-		"TOUCHSCREEN_GUI",
 	},
 }
 
@@ -80,11 +79,5 @@ files["builtin/common/tests"] = {
 		"describe",
 		"it",
 		"assert",
-	},
-}
-
-files["builtin/fstk"] = {
-	read_globals = {
-		"TOUCHSCREEN_GUI",
 	},
 }

builtin/fstk/buttonbar.lua
@@ -18,7 +18,9 @@
 
 
 local BASE_SPACING = 0.1
-local SCROLL_BTN_WIDTH = TOUCHSCREEN_GUI and 0.8 or 0.5
+local function get_scroll_btn_width()
+	return core.settings:get_bool("enable_touch") and 0.8 or 0.5
+end
 
 local function buttonbar_formspec(self)
 	if self.hidden then
@@ -39,7 +41,7 @@ local function buttonbar_formspec(self)
 
 	-- The number of buttons per page is always calculated as if the scroll
     -- buttons were visible.
-	local avail_space = self.size.x - 2*BASE_SPACING - 2*SCROLL_BTN_WIDTH
+	local avail_space = self.size.x - 2*BASE_SPACING - 2*get_scroll_btn_width()
 	local btns_per_page = math.floor((avail_space - BASE_SPACING) / (btn_size + BASE_SPACING))
 
 	self.num_pages = math.ceil(#self.buttons / btns_per_page)
@@ -55,7 +57,7 @@ local function buttonbar_formspec(self)
 
 	local btn_start_x = self.pos.x + btn_spacing
 	if show_scroll_btns then
-		btn_start_x = btn_start_x + BASE_SPACING + SCROLL_BTN_WIDTH
+		btn_start_x = btn_start_x + BASE_SPACING + get_scroll_btn_width()
 	end
 
 	for i = first_btn, first_btn + btns_per_page - 1 do
@@ -80,7 +82,7 @@ local function buttonbar_formspec(self)
 			y = self.pos.y + BASE_SPACING,
 		}
 		local btn_next_pos = {
-			x = self.pos.x + self.size.x - BASE_SPACING - SCROLL_BTN_WIDTH,
+			x = self.pos.x + self.size.x - BASE_SPACING - get_scroll_btn_width(),
 			y = self.pos.y + BASE_SPACING,
 		}
 
@@ -88,11 +90,11 @@ local function buttonbar_formspec(self)
 				self.btn_prev_name, self.btn_next_name))
 
 		table.insert(formspec, string.format("button[%f,%f;%f,%f;%s;<]",
-				btn_prev_pos.x, btn_prev_pos.y, SCROLL_BTN_WIDTH, btn_size,
+				btn_prev_pos.x, btn_prev_pos.y, get_scroll_btn_width(), btn_size,
 				self.btn_prev_name))
 
 		table.insert(formspec, string.format("button[%f,%f;%f,%f;%s;>]",
-				btn_next_pos.x, btn_next_pos.y, SCROLL_BTN_WIDTH, btn_size,
+				btn_next_pos.x, btn_next_pos.y, get_scroll_btn_width(), btn_size,
 				self.btn_next_name))
 	end
 

builtin/mainmenu/content/dlg_contentstore.lua
@@ -898,7 +898,7 @@ local function get_info_formspec(text)
 	return table.concat({
 		"formspec_version[6]",
 		"size[15.75,9.5]",
-		TOUCHSCREEN_GUI and "padding[0.01,0.01]" or "position[0.5,0.55]",
+		core.settings:get_bool("enable_touch") and "padding[0.01,0.01]" or "position[0.5,0.55]",
 
 		"label[4,4.35;", text, "]",
 		"container[0,", H - 0.8 - 0.375, "]",
@@ -928,7 +928,7 @@ function store.get_formspec(dlgdata)
 	local formspec = {
 		"formspec_version[6]",
 		"size[15.75,9.5]",
-		TOUCHSCREEN_GUI and "padding[0.01,0.01]" or "position[0.5,0.55]",
+		core.settings:get_bool("enable_touch") and "padding[0.01,0.01]" or "position[0.5,0.55]",
 
 		"style[status,downloading,queued;border=false]",
 
@@ -1175,8 +1175,8 @@ end
 
 function store.handle_events(event)
 	if event == "DialogShow" then
-		-- On mobile, don't show the "MINETEST" header behind the dialog.
-		mm_game_theme.set_engine(TOUCHSCREEN_GUI)
+		-- On touchscreen, don't show the "MINETEST" header behind the dialog.
+		mm_game_theme.set_engine(core.settings:get_bool("enable_touch"))
 
 		-- If the store is already loaded, auto-install packages here.
 		do_auto_install()

builtin/mainmenu/settings/dlg_settings.lua
@@ -316,8 +316,8 @@ local function check_requirements(name, requires)
 	local special = {
 		android = PLATFORM == "Android",
 		desktop = PLATFORM ~= "Android",
-		touchscreen_gui = TOUCHSCREEN_GUI,
-		keyboard_mouse = not TOUCHSCREEN_GUI,
+		touchscreen_gui = core.settings:get_bool("enable_touch"),
+		keyboard_mouse = not core.settings:get_bool("enable_touch"),
 		shaders_support = shaders_support,
 		shaders = core.settings:get_bool("enable_shaders") and shaders_support,
 		opengl = video_driver == "opengl",
@@ -449,13 +449,13 @@ local function get_formspec(dialogdata)
 
 	local extra_h = 1 -- not included in tabsize.height
 	local tabsize = {
-		width = TOUCHSCREEN_GUI and 16.5 or 15.5,
-		height = TOUCHSCREEN_GUI and (10 - extra_h) or 12,
+		width = core.settings:get_bool("enable_touch") and 16.5 or 15.5,
+		height = core.settings:get_bool("enable_touch") and (10 - extra_h) or 12,
 	}
 
-	local scrollbar_w = TOUCHSCREEN_GUI and 0.6 or 0.4
+	local scrollbar_w = core.settings:get_bool("enable_touch") and 0.6 or 0.4
 
-	local left_pane_width = TOUCHSCREEN_GUI and 4.5 or 4.25
+	local left_pane_width = core.settings:get_bool("enable_touch") and 4.5 or 4.25
 	local search_width = left_pane_width + scrollbar_w - (0.75 * 2)
 
 	local back_w = 3
@@ -468,7 +468,7 @@ local function get_formspec(dialogdata)
 	local fs = {
 		"formspec_version[6]",
 		"size[", tostring(tabsize.width), ",", tostring(tabsize.height + extra_h), "]",
-		TOUCHSCREEN_GUI and "padding[0.01,0.01]" or "",
+		core.settings:get_bool("enable_touch") and "padding[0.01,0.01]" or "",
 		"bgcolor[#0000]",
 
 		-- HACK: this is needed to allow resubmitting the same formspec
@@ -641,11 +641,22 @@ local function buttonhandler(this, fields)
 		local value = core.is_yes(fields.show_advanced)
 		core.settings:set_bool("show_advanced", value)
 		write_settings_early()
+	end
 
+	-- enable_touch is a checkbox in a setting component. We handle this
+	-- setting differently so we can hide/show pages using the next if-statement
+	if fields.enable_touch ~= nil then
+		local value = core.is_yes(fields.enable_touch)
+		core.settings:set_bool("enable_touch", value)
+		write_settings_early()
+	end
+
+	if fields.show_advanced ~= nil or fields.enable_touch ~= nil then
 		local suggested_page_id = update_filtered_pages(dialogdata.query)
 
+		dialogdata.components = nil
+
 		if not filtered_page_by_id[dialogdata.page_id] then
-			dialogdata.components = nil
 			dialogdata.leftscroll = 0
 			dialogdata.rightscroll = 0
 

builtin/mainmenu/tab_local.lua
@@ -94,7 +94,7 @@ function singleplayer_refresh_gamebar()
 
 	local btnbar = buttonbar_create(
 			"game_button_bar",
-			TOUCHSCREEN_GUI and {x = 0, y = 7.25} or {x = 0, y = 7.475},
+			core.settings:get_bool("enable_touch") and {x = 0, y = 7.25} or {x = 0, y = 7.475},
 			{x = 15.5, y = 1.25},
 			"#000000",
 			game_buttonbar_button_handler)

src/CMakeLists.txt
@@ -109,9 +109,10 @@ if(BUILD_CLIENT AND ENABLE_SOUND)
 	endif()
 endif()
 
-option(ENABLE_TOUCH "Enable Touchscreen support" FALSE)
+option(ENABLE_TOUCH "Enable touchscreen by default" FALSE)
 if(ENABLE_TOUCH)
-	add_definitions(-DHAVE_TOUCHSCREENGUI)
+	message(STATUS "Touchscreen support enabled by default.")
+	add_definitions(-DENABLE_TOUCH)
 endif()
 
 if(BUILD_CLIENT)

src/client/clientlauncher.cpp
@@ -249,10 +249,10 @@ bool ClientLauncher::run(GameStartData &start_data, const Settings &cmd_args)
 			m_rendering_engine->get_video_driver()->setTextureCreationFlag(
 					video::ETCF_CREATE_MIP_MAPS, g_settings->getBool("mip_map"));
 
-#ifdef HAVE_TOUCHSCREENGUI
-			receiver->m_touchscreengui = new TouchScreenGUI(m_rendering_engine->get_raw_device(), receiver);
-			g_touchscreengui = receiver->m_touchscreengui;
-#endif
+			if (g_settings->getBool("enable_touch")) {
+				receiver->m_touchscreengui = new TouchScreenGUI(m_rendering_engine->get_raw_device(), receiver);
+				g_touchscreengui = receiver->m_touchscreengui;
+			}
 
 			the_game(
 				kill,
@@ -283,11 +283,11 @@ bool ClientLauncher::run(GameStartData &start_data, const Settings &cmd_args)
 
 		m_rendering_engine->get_scene_manager()->clear();
 
-#ifdef HAVE_TOUCHSCREENGUI
-		delete g_touchscreengui;
-		g_touchscreengui = NULL;
-		receiver->m_touchscreengui = NULL;
-#endif
+		if (g_touchscreengui) {
+			delete g_touchscreengui;
+			g_touchscreengui = NULL;
+			receiver->m_touchscreengui = NULL;
+		}
 
 		/* Save the settings when leaving the game.
 		 * This makes sure that setting changes made in-game are persisted even

commit 3ba4ee02becf14adfeec9d12d8d60ef781c4a456

Touchscreen: Fix virtual joystick sometimes going backwards
This fixes a regression introduced by 0671a392be92bc06ab1f33bc47d3bddd6fa72c20 / #14075.

src/client/inputhandler.cpp
@@ -237,7 +237,9 @@ float RealInputHandler::getMovementDirection()
 
 	if (x != 0 || z != 0) /* If there is a keyboard event, it takes priority */
 		return std::atan2(x, z);
-	else if (g_touchscreengui && g_touchscreengui->getMovementDirection())
+	// `getMovementDirection() == 0` means forward, so we cannot use
+	// `getMovementDirection()` as a condition.
+	else if (g_touchscreengui && g_touchscreengui->getMovementSpeed())
 		return g_touchscreengui->getMovementDirection();
 	return joystick.getMovementDirection();
 }