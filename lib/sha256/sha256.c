Desour/minetest

Move sha256.c to lib/
Precompiled headers don't work if we're not a pure C++ project.

lib/sha256/sha256.c
@@ -7,7 +7,7 @@
 # include <stdlib.h>
 # include <string.h>
 
-# include <util/sha2.h>
+# include "sha256/sha2.h"
 
 #  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2a 19 Mar 2015"
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT

src/util/srp.cpp
@@ -52,7 +52,7 @@
 	#include <mini-gmp.h>
 #endif
 
-#include <util/sha2.h>
+#include <sha256/sha2.h>
 
 #include "srp.h"
 //#define CSRP_USE_SHA1

billjamesevans/ai-native-luanti

lib/sha256/sha256.c
@@ -56,17 +56,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "util/sha256.h"
+#include "sha256/sha256.h"
 
 #if defined(_MSC_VER) && !defined(__clang__) && !defined(__attribute__)
 #define __attribute__(a)
 #endif
 
-/* pull HAVE_ENDIAN_H from Minetest */
-#include "config.h"
-#if !HAVE_ENDIAN_H
-#undef HAVE_ENDIAN_H
-#endif
+#include "cmake_config.h" /* HAVE_ENDIAN_H */
 
 /** endian.h **/
 /*

src/util/srp.cpp
@@ -50,7 +50,7 @@
 	#include <mini-gmp.h>
 #endif
 
-#include "util/sha256.h"
+#include <sha256/sha256.h>
 
 #include "srp.h"
 //#define CSRP_USE_SHA1