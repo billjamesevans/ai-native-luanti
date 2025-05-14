billjamesevans/ai-native-luanti

commit c04da5906d83c4931131fb885161404e21459832

Purge some dead code (mostly Irrlicht) (#16111)
* Remove obsolete Irrlicht attributes system

* Remove dead GUI element types

* Remove some obsolete Irrlicht headers

* Fix some oopsies from 7655aad

src/gui/guiTable.cpp
@@ -635,11 +635,6 @@ void GUITable::setDynamicData(const DynamicData &dyndata)
 	m_scrollbar->setPos(dyndata.scrollpos);
 }
 
-const c8* GUITable::getTypeName() const
-{
-	return "GUITable";
-}
-
 void GUITable::updateAbsolutePosition()
 {
 	IGUIElement::updateAbsolutePosition();

irr/LICENSE
@@ -21,6 +21,4 @@ Copyright (C) 2002-2012 Nikolaus Gebhardt
   the Irrlicht Engine in your product, you must acknowledge somewhere in your
   documentation that you've used the IJPG code. It would also be nice to mention
   that you use the Irrlicht Engine, the zlib, libPng and aesGladman. See the
-  corresponding license files for further informations. It is also possible to disable
-  usage of those additional libraries by defines in the IrrCompileConfig.h header and
-  recompiling the engine.
+  corresponding license files for further informations.

irr/include/EAttributes.h
@@ -1,32 +1,0 @@
-// Copyright (C) 2002-2012 Nikolaus Gebhardt
-// This file is part of the "Irrlicht Engine".
-// For conditions of distribution and use, see copyright notice in irrlicht.h
-
-#pragma once
-
-namespace irr
-{
-namespace io
-{
-
-//! Types of attributes available for IAttributes
-enum E_ATTRIBUTE_TYPE
-{
-	// integer attribute
-	EAT_INT = 0,
-
-	// float attribute
-	EAT_FLOAT,
-
-	// boolean attribute
-	EAT_BOOL,
-
-	// known attribute type count
-	EAT_COUNT,
-
-	// unknown attribute
-	EAT_UNKNOWN
-};
-
-} // end namespace io
-} // end namespace irr

irr/include/EGUIElementTypes.h
@@ -24,9 +24,6 @@ enum EGUI_ELEMENT_TYPE
 	//! A combo box (IGUIComboBox)
 	EGUIET_COMBO_BOX,
 
-	//! A context menu (IGUIContextMenu)
-	EGUIET_CONTEXT_MENU,
-
 	//! A menu (IGUIMenu)
 	EGUIET_MENU,
 
@@ -36,35 +33,20 @@ enum EGUI_ELEMENT_TYPE
 	//! A file open dialog (IGUIFileOpenDialog)
 	EGUIET_FILE_OPEN_DIALOG,
 
-	//! A color select open dialog (IGUIColorSelectDialog)
-	EGUIET_COLOR_SELECT_DIALOG,
-
-	//! A in/out fader (IGUIInOutFader)
-	EGUIET_IN_OUT_FADER,
-
 	//! An image (IGUIImage)
 	EGUIET_IMAGE,
 
 	//! A list box (IGUIListBox)
 	EGUIET_LIST_BOX,
 
-	//! A mesh viewer (IGUIMeshViewer)
-	EGUIET_MESH_VIEWER,
-
-	//! A message box (IGUIWindow)
-	EGUIET_MESSAGE_BOX,
-
-	//! A modal screen
-	EGUIET_MODAL_SCREEN,
-
 	//! A scroll bar (IGUIScrollBar)
 	EGUIET_SCROLL_BAR,
 
-	//! A spin box (IGUISpinBox)
-	EGUIET_SPIN_BOX,
-
 	//! A static text (IGUIStaticText)
 	EGUIET_STATIC_TEXT,
+
+	//! A table (GUITable)
+	EGUIET_TABLE,
 
 	//! A tab (IGUITab)
 	EGUIET_TAB,
@@ -72,17 +54,8 @@ enum EGUI_ELEMENT_TYPE
 	//! A tab control
 	EGUIET_TAB_CONTROL,
 
-	//! A Table
-	EGUIET_TABLE,
-
 	//! A tool bar (IGUIToolBar)
 	EGUIET_TOOL_BAR,
-
-	//! A Tree View
-	EGUIET_TREE_VIEW,
-
-	//! A window
-	EGUIET_WINDOW,
 
 	//! Unknown type.
 	EGUIET_ELEMENT,
@@ -97,37 +70,6 @@ enum EGUI_ELEMENT_TYPE
 	EGUIET_FORCE_32_BIT = 0x7fffffff
 
 };
-
-//! Names for built-in element types
-const c8 *const GUIElementTypeNames[] = {
-		"button",
-		"checkBox",
-		"comboBox",
-		"contextMenu",
-		"menu",
-		"editBox",
-		"fileOpenDialog",
-		"colorSelectDialog",
-		"inOutFader",
-		"image",
-		"listBox",
-		"meshViewer",
-		"messageBox",
-		"modalScreen",
-		"scrollBar",
-		"spinBox",
-		"staticText",
-		"tab",
-		"tabControl",
-		"table",
-		"toolBar",
-		"treeview",
-		"window",
-		"element",
-		"root",
-		"profiler",
-		0,
-	};
 
 } // end namespace gui
 } // end namespace irr

irr/include/IAttributes.h
@@ -1,87 +1,0 @@
-// Copyright (C) 2002-2012 Nikolaus Gebhardt
-// This file is part of the "Irrlicht Engine".
-// For conditions of distribution and use, see copyright notice in irrlicht.h
-
-#pragma once
-
-#include "IReferenceCounted.h"
-#include "EAttributes.h"
-
-namespace irr
-{
-namespace video
-{
-class ITexture;
-} // end namespace video
-namespace io
-{
-
-//! Provides a generic interface for attributes and their values and the possibility to serialize them
-class IAttributes : public virtual IReferenceCounted
-{
-public:
-	//! Returns the type of an attribute
-	//! \param attributeName: Name for the attribute
-	virtual E_ATTRIBUTE_TYPE getAttributeType(const c8 *attributeName) const = 0;
-
-	//! Returns if an attribute with a name exists
-	virtual bool existsAttribute(const c8 *attributeName) const = 0;
-
-	//! Removes all attributes
-	virtual void clear() = 0;
-
-	/*
-
-		Integer Attribute
-
-	*/
-
-	//! Adds an attribute as integer
-	virtual void addInt(const c8 *attributeName, s32 value) = 0;
-
-	//! Sets an attribute as integer value
-	virtual void setAttribute(const c8 *attributeName, s32 value) = 0;
-
-	//! Gets an attribute as integer value
-	//! \param attributeName: Name of the attribute to get.
-	//! \param defaultNotFound Value returned when attributeName was not found
-	//! \return Returns value of the attribute previously set by setAttribute()
-	virtual s32 getAttributeAsInt(const c8 *attributeName, irr::s32 defaultNotFound = 0) const = 0;
-
-	/*
-
-		Float Attribute
-
-	*/
-
-	//! Adds an attribute as float
-	virtual void addFloat(const c8 *attributeName, f32 value) = 0;
-
-	//! Sets a attribute as float value
-	virtual void setAttribute(const c8 *attributeName, f32 value) = 0;
-
-	//! Gets an attribute as float value
-	//! \param attributeName: Name of the attribute to get.
-	//! \param defaultNotFound Value returned when attributeName was not found
-	//! \return Returns value of the attribute previously set by setAttribute()
-	virtual f32 getAttributeAsFloat(const c8 *attributeName, irr::f32 defaultNotFound = 0.f) const = 0;
-
-	/*
-		Bool Attribute
-	*/
-
-	//! Adds an attribute as bool
-	virtual void addBool(const c8 *attributeName, bool value) = 0;
-
-	//! Sets an attribute as boolean value
-	virtual void setAttribute(const c8 *attributeName, bool value) = 0;
-
-	//! Gets an attribute as boolean value
-	//! \param attributeName: Name of the attribute to get.
-	//! \param defaultNotFound Value returned when attributeName was not found
-	//! \return Returns value of the attribute previously set by setAttribute()
-	virtual bool getAttributeAsBool(const c8 *attributeName, bool defaultNotFound = false) const = 0;
-};
-
-} // end namespace io
-} // end namespace irr

irr/include/IFileSystem.h
@@ -19,7 +19,6 @@ namespace io
 class IReadFile;
 class IWriteFile;
 class IFileList;
-class IAttributes;
 
 //! The FileSystem manages files and archives and provides access to them.
 /** It manages where files are, so that modules which use the the IO do not

irr/include/IGUIElement.h
@@ -662,13 +662,6 @@ public:
 		return type == Type;
 	}
 
-	//! Returns the type name of the gui element.
-	/** This is needed serializing elements. */
-	virtual const c8 *getTypeName() const
-	{
-		return GUIElementTypeNames[Type];
-	}
-
 	//! Returns the name of the element.
 	/** \return Name as character string. */
 	virtual const c8 *getName() const

irr/include/ISceneManager.h
@@ -10,7 +10,6 @@
 #include "dimension2d.h"
 #include "SColor.h"
 #include "ESceneNodeTypes.h"
-#include "SceneParameters.h" // IWYU pragma: export
 
 namespace irr
 {
@@ -20,7 +19,6 @@ struct SEvent;
 namespace io
 {
 class IReadFile;
-class IAttributes;
 class IWriteFile;
 class IFileSystem;
 } // end namespace io
@@ -372,11 +370,6 @@ public:
 	//! Clears the whole scene.
 	/** All scene nodes are removed. */
 	virtual void clear() = 0;
-
-	//! Get interface to the parameters set in this scene.
-	/** String parameters can be used by plugins and mesh loaders.
-	See	COLLADA_CREATE_SCENE_INSTANCES and DMF_USE_MATERIALS_DIRS */
-	virtual io::IAttributes *getParameters() = 0;
 
 	//! Get current render pass.
 	/** All scene nodes are being rendered in a specific order.

commit 67acfef02a23c891efbe8e662add44866b14f236

Fix table[] focus regression from c04da59 (#16136)

src/gui/guiTable.cpp
@@ -32,7 +32,7 @@ GUITable::GUITable(gui::IGUIEnvironment *env,
 		core::rect<s32> rectangle,
 		ISimpleTextureSource *tsrc
 ):
-	gui::IGUIElement(gui::EGUIET_ELEMENT, env, parent, id, rectangle),
+	gui::IGUIElement(gui::EGUIET_TABLE, env, parent, id, rectangle),
 	m_tsrc(tsrc)
 {
 	assert(tsrc != NULL);