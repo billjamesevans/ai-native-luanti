billjamesevans/ai-native-luanti

commit e13a45edbbbe8059d0bd16588feac6b44990872f

Fix problem with deterministic std::stable_sort (#16987)
caused a SIGABRT when compiled with MacOSX26.2.sdk

src/gui/guiFormSpecMenu.cpp
@@ -3360,8 +3360,11 @@ void GUIFormSpecMenu::legacySortElements(std::list<IGUIElement *>::iterator from
 		// TODO: getSpecByID is a linear search. It should made O(1), or cached here.
 		const FieldSpec *spec_a = getSpecByID(a->getID());
 		const FieldSpec *spec_b = getSpecByID(b->getID());
-		return spec_a && spec_b &&
-			spec_a->priority < spec_b->priority;
+		// Have to be deterministic also for elements without spec
+		if (spec_a && spec_b)
+			return spec_a->priority < spec_b->priority;
+
+		return a->getID() < b->getID();
 	});
 
 	// 3: Re-assign the pointers

commit 19accd92f3721de59608bd7842419450fcb92da7

Fix missing scrollbar in world mod selection dialog
Fixes a regression introduced by e13a45edb.

src/gui/guiFormSpecMenu.cpp
@@ -3360,11 +3360,14 @@ void GUIFormSpecMenu::legacySortElements(std::list<IGUIElement *>::iterator from
 		// TODO: getSpecByID is a linear search. It should made O(1), or cached here.
 		const FieldSpec *spec_a = getSpecByID(a->getID());
 		const FieldSpec *spec_b = getSpecByID(b->getID());
-		// Have to be deterministic also for elements without spec
+		// The comparison has to be compatible with strict weak ordering
 		if (spec_a && spec_b)
 			return spec_a->priority < spec_b->priority;
 
-		return a->getID() < b->getID();
+		if (spec_a && !spec_b)
+			return true;
+
+		return false;
 	});
 
 	// 3: Re-assign the pointers