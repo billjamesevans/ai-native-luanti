luanti-org/luanti

Support mouse buttons beyond X2

irr/src/CIrrDeviceSDL.cpp
@@ -100,21 +100,6 @@
 	#define SDL_FINGER_ID(ev) ((ev).tfinger.fingerId)
 #endif
 
-// Since SDL doesn't have mouse keys as keycodes we need to fall back to EKEY_CODE in some cases.
-static inline bool is_fake_key(EKEY_CODE key) {
-	switch (key) {
-	case KEY_LBUTTON:
-	case KEY_MBUTTON:
-	case KEY_RBUTTON:
-	case KEY_XBUTTON1:
-	case KEY_XBUTTON2:
-		return true;
-
-	default:
-		return false;
-	}
-}
-
 static int SDLDeviceInstances = 0;
 
 #ifdef _IRR_EMSCRIPTEN_PLATFORM_
@@ -306,13 +291,10 @@ wchar_t CIrrDeviceSDL::findCharToPassToIrrlicht(uint32_t sdlKey, EKEY_CODE irrli
 	}
 }
 
-std::variant<u32, EKEY_CODE> CIrrDeviceSDL::getScancodeFromKey(const Keycode &key) const
+u32 CIrrDeviceSDL::getScancodeFromKey(const Keycode &key) const
 {
 	u32 keynum = 0;
 	if (const auto *keycode = std::get_if<EKEY_CODE>(&key)) {
-		// Fake keys (e.g. mouse buttons): use EKEY_CODE since there is no corresponding scancode.
-		if (is_fake_key(*keycode))
-			return *keycode;
 		// Try to convert the EKEY_CODE to a SDL scancode.
 		for (const auto &entry: KeyMap) {
 			if (entry.second == *keycode) {
@@ -326,14 +308,14 @@ std::variant<u32, EKEY_CODE> CIrrDeviceSDL::getScancodeFromKey(const Keycode &ke
 
 	// SDL3 returns a valid scancode for keycode 0. This is undesired.
 	if (keynum == 0)
-		return (u32) 0;
+		return 0;
 
 #ifdef _IRR_USE_SDL3_
 	SDL_Keymod kmod = SDL_KMOD_NONE; // TODO: respect modifiers
-	return (u32)SDL_GetScancodeFromKey(keynum, &kmod);
+	return SDL_GetScancodeFromKey(keynum, &kmod);
 #else
 	// Modifiers not supported
-	return (u32)SDL_GetScancodeFromKey(keynum);
+	return SDL_GetScancodeFromKey(keynum);
 #endif
 }
 
@@ -940,54 +922,34 @@ bool CIrrDeviceSDL::run()
 			if (button == SDL_BUTTON_X2)
 				button = SDL_BUTTON_RIGHT;
 #endif
+			irrevent.MouseInput.Button = button;
+			auto is_down = SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN;
+
+			if (is_down)
+				MouseButtonStates |= SDL_BUTTON_MASK(button);
+			else
+				MouseButtonStates &= ~SDL_BUTTON_MASK(button);
+
 			switch (button) {
 			case SDL_BUTTON_LEFT:
-				if (SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-					irrevent.MouseInput.Event = EMIE_LMOUSE_PRESSED_DOWN;
-					MouseButtonStates |= EMBSM_LEFT;
-				} else {
-					irrevent.MouseInput.Event = EMIE_LMOUSE_LEFT_UP;
-					MouseButtonStates &= ~EMBSM_LEFT;
-				}
+				irrevent.MouseInput.Event = is_down ? EMIE_LMOUSE_PRESSED_DOWN : EMIE_LMOUSE_LEFT_UP;
 				break;
 
 			case SDL_BUTTON_RIGHT:
-				if (SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-					irrevent.MouseInput.Event = EMIE_RMOUSE_PRESSED_DOWN;
-					MouseButtonStates |= EMBSM_RIGHT;
-				} else {
-					irrevent.MouseInput.Event = EMIE_RMOUSE_LEFT_UP;
-					MouseButtonStates &= ~EMBSM_RIGHT;
-				}
+				irrevent.MouseInput.Event = is_down ? EMIE_RMOUSE_PRESSED_DOWN : EMIE_RMOUSE_LEFT_UP;
 				break;
 
 			case SDL_BUTTON_MIDDLE:
-				if (SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-					irrevent.MouseInput.Event = EMIE_MMOUSE_PRESSED_DOWN;
-					MouseButtonStates |= EMBSM_MIDDLE;
-				} else {
-					irrevent.MouseInput.Event = EMIE_MMOUSE_LEFT_UP;
-					MouseButtonStates &= ~EMBSM_MIDDLE;
-				}
+				irrevent.MouseInput.Event = is_down ? EMIE_MMOUSE_PRESSED_DOWN : EMIE_MMOUSE_LEFT_UP;
 				break;
 
-			// Since Irrlicht does not have event types for X1/X2 buttons, we simply pass
-			// those as keycodes instead. This is relatively hacky but avoids the effort of
-			// adding more mouse events that will be discarded anyway once we switch to SDL
-			case SDL_BUTTON_X1:
-				irrevent.EventType = EET_KEY_INPUT_EVENT;
-				irrevent.KeyInput.Key = KEY_XBUTTON1;
-				break;
-
-			case SDL_BUTTON_X2:
-				irrevent.EventType = EET_KEY_INPUT_EVENT;
-				irrevent.KeyInput.Key = KEY_XBUTTON2;
-				break;
+			default: // Other mouse buttons do not have dedicated events:
+				irrevent.MouseInput.Event = is_down ? EMIE_OMOUSE_PRESSED_DOWN : EMIE_OMOUSE_LEFT_UP;
 			}
 
 			bool shift = (keymod & SDL_KMOD_SHIFT) != 0;
 			bool control = (keymod & SDL_KMOD_CTRL) != 0;
-			if (irrevent.EventType == EET_MOUSE_INPUT_EVENT && irrevent.MouseInput.Event != EMIE_MOUSE_MOVED) {
+			if (irrevent.MouseInput.Event != EMIE_MOUSE_MOVED) {
 				irrevent.MouseInput.ButtonStates = MouseButtonStates;
 				irrevent.MouseInput.X = static_cast<s32>(SDL_event.button.x * ScaleX);
 				irrevent.MouseInput.Y = static_cast<s32>(SDL_event.button.y * ScaleY);
@@ -1005,12 +967,6 @@ bool CIrrDeviceSDL::run()
 						postEventFromUser(irrevent);
 					}
 				}
-			} else if (irrevent.EventType == EET_KEY_INPUT_EVENT) {
-				irrevent.KeyInput.Char = 0;
-				irrevent.KeyInput.PressedDown = SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN;
-				irrevent.KeyInput.Shift = shift;
-				irrevent.KeyInput.Control = control;
-				postEventFromUser(irrevent);
 			}
 			break;
 		}

src/client/keycode.cpp
@@ -74,13 +74,8 @@ static std::vector<table_key> table = {
 
 	// Keys without a Char
 	// Note: we add "Key" to the description if the string could be confused for something else
-	DEFINEKEY1(KEY_LBUTTON, N_("Left Click"))
-	DEFINEKEY1(KEY_RBUTTON, N_("Right Click"))
 	//~ Usually paired with the Pause key
 	DEFINEKEY1(KEY_CANCEL, N_("Break Key"))
-	DEFINEKEY1(KEY_MBUTTON, N_("Middle Click"))
-	DEFINEKEY1(KEY_XBUTTON1, N_("Mouse X1"))
-	DEFINEKEY1(KEY_XBUTTON2, N_("Mouse X2"))
 	DEFINEKEY1(KEY_BACK, N_("Backspace"))
 	DEFINEKEY1(KEY_TAB, N_("Tab Key"))
 	DEFINEKEY1(KEY_CLEAR, N_("Clear Key"))
@@ -270,36 +265,25 @@ static const table_key &lookup_keyname(std::string_view name)
 
 static const table_key &lookup_scancode(const u32 scancode)
 {
+	if (!scancode)
+		return invalid_key;
 	auto key = RenderingEngine::get_raw_device()->getKeyFromScancode(scancode);
 	return std::holds_alternative<EKEY_CODE>(key) ?
 		lookup_keykey(std::get<EKEY_CODE>(key)) :
 		lookup_keychar(std::get<wchar_t>(key));
 }
 
-const table_key &KeyPress::lookupScancode() const
-{
-	switch (getType()) {
-	case SCANCODE_INPUT:
-		return lookup_scancode(std::get<SCANCODE_INPUT>(value));
-	case LEGACY_KEYCODE_INPUT:
-		return lookup_keykey(std::get<LEGACY_KEYCODE_INPUT>(value));
-	default:
-		return invalid_key;
-	}
-}
-
 void KeyPress::loadFromKey(EKEY_CODE keycode, wchar_t keychar)
 {
-	auto irr_scancode = RenderingEngine::get_raw_device()->getScancodeFromKey(Keycode(keycode, keychar));
-	if (auto scancode = std::get_if<u32>(&irr_scancode))
-		value.emplace<SCANCODE_INPUT>(*scancode);
-	else
-		value.emplace<LEGACY_KEYCODE_INPUT>(std::get<EKEY_CODE>(irr_scancode));
+	auto scancode = RenderingEngine::get_raw_device()->getScancodeFromKey(Keycode(keycode, keychar));
+	value.emplace<SCANCODE_INPUT>(scancode);
 }
 
 KeyPress::KeyPress(const std::string &name)
 {
-	if (loadFromScancode(name))
+	if (loadUnsignedFromPrefix<SCANCODE_INPUT>(name, "SYSTEM_SCANCODE_"))
+		return;
+	if (loadUnsignedFromPrefix<MOUSE_BUTTON_INPUT>(name, "MOUSE_BUTTON_"))
 		return;
 	const auto &key = lookup_keyname(name);
 	loadFromKey(key.Key, key.Char);
@@ -310,7 +294,25 @@ KeyPress::KeyPress(const SEvent::SKeyInput &in)
 	if (in.SystemKeyCode)
 		value.emplace<SCANCODE_INPUT>(in.SystemKeyCode);
 	else
-		value.emplace<LEGACY_KEYCODE_INPUT>(in.Key);
+		loadFromKey(in.Key, in.Char);
+}
+
+KeyPress::KeyPress(const SEvent::SMouseInput &in)
+{
+	switch (in.Event) {
+	case EMIE_LMOUSE_PRESSED_DOWN:
+	case EMIE_MMOUSE_PRESSED_DOWN:
+	case EMIE_RMOUSE_PRESSED_DOWN:
+	case EMIE_OMOUSE_PRESSED_DOWN:
+	case EMIE_LMOUSE_LEFT_UP:
+	case EMIE_MMOUSE_LEFT_UP:
+	case EMIE_RMOUSE_LEFT_UP:
+	case EMIE_OMOUSE_LEFT_UP:
+		value.emplace<MOUSE_BUTTON_INPUT>(in.Button);
+		break;
+	default:
+		value.emplace<MOUSE_BUTTON_INPUT>(0);
+	}
 }
 
 std::string KeyPress::sym() const
@@ -318,8 +320,8 @@ std::string KeyPress::sym() const
 	switch (getType()) {
 	case SCANCODE_INPUT:
 		return "SYSTEM_SCANCODE_" + std::to_string(std::get<SCANCODE_INPUT>(value));
-	case LEGACY_KEYCODE_INPUT:
-		return lookupScancode().Name;
+	case MOUSE_BUTTON_INPUT:
+		return "MOUSE_BUTTON_" + std::to_string(std::get<MOUSE_BUTTON_INPUT>(value));
 	default:
 		return "";
 	}
@@ -328,29 +330,47 @@ std::string KeyPress::sym() const
 std::string KeyPress::name() const
 {
 	switch (getType()) {
-	case SCANCODE_INPUT:
-	case LEGACY_KEYCODE_INPUT: {
-		const auto &name = lookupScancode().LangName;
+	case SCANCODE_INPUT: {
+		auto scancode = getScancode();
+		const auto &name = lookup_scancode(scancode).LangName;
 		if (!name.empty())
 			return strgettext(name);
-		if (auto scancode = getScancode())
+		if (scancode)
 			return fmtgettext("Scancode: %d", scancode);
 		return "";
+	}
+	case MOUSE_BUTTON_INPUT: {
+		auto button = std::get<MOUSE_BUTTON_INPUT>(value);
+		switch (button) {
+		case SDL_BUTTON_LEFT:
+			return strgettext("Left Click");
+		case SDL_BUTTON_MIDDLE:
+			return strgettext("Middle Click");
+		case SDL_BUTTON_RIGHT:
+			return strgettext("Right Click");
+		case SDL_BUTTON_X1:
+			return strgettext("Mouse X1");
+		case SDL_BUTTON_X2:
+			return strgettext("Mouse X2");
+		default:
+			return fmtgettext("Mouse Button %d", button);
+		}
 	}
 	default:
 		return "";
 	}
 }
 
-bool KeyPress::loadFromScancode(const std::string &name)
+template<std::size_t I>
+bool KeyPress::loadUnsignedFromPrefix(const std::string &name, const std::string &prefix)
 {
-	if (!str_starts_with(name, "SYSTEM_SCANCODE_"))
+	if (!str_starts_with(name, prefix))
 		return false;
 	char *p;
-	const auto code = strtoul(name.c_str()+16, &p, 10);
+	const auto code = strtoul(name.c_str()+prefix.size(), &p, 10);
 	if (p != name.c_str() + name.size())
 		return false;
-	value.emplace<SCANCODE_INPUT>(code);
+	value.emplace<I>(code);
 	return true;
 }
 
@@ -359,8 +379,8 @@ KeyPress::operator bool() const
 	switch (getType()) {
 	case SCANCODE_INPUT:
 		return std::get<SCANCODE_INPUT>(value) != 0;
-	case LEGACY_KEYCODE_INPUT:
-		return Keycode::isValid(std::get<LEGACY_KEYCODE_INPUT>(value));
+	case MOUSE_BUTTON_INPUT:
+		return std::get<MOUSE_BUTTON_INPUT>(value) != 0;
 	case GAME_ACTION_INPUT:
 		return std::get<GAME_ACTION_INPUT>(value) < KeyType::INTERNAL_ENUM_COUNT;
 	default:

billjamesevans/ai-native-luanti

Co-authored-by: SmallJoker <SmallJoker@users.noreply.github.com>

irr/src/CIrrDeviceSDL.cpp
@@ -102,21 +102,6 @@
 	#define SDL_FINGER_ID(ev) ((ev).tfinger.fingerId)
 #endif
 
-// Since SDL doesn't have mouse keys as keycodes we need to fall back to EKEY_CODE in some cases.
-static inline bool is_fake_key(EKEY_CODE key) {
-	switch (key) {
-	case KEY_LBUTTON:
-	case KEY_MBUTTON:
-	case KEY_RBUTTON:
-	case KEY_XBUTTON1:
-	case KEY_XBUTTON2:
-		return true;
-
-	default:
-		return false;
-	}
-}
-
 static int SDLDeviceInstances = 0;
 
 #ifdef _IRR_EMSCRIPTEN_PLATFORM_
@@ -308,13 +293,10 @@ wchar_t CIrrDeviceSDL::findCharToPassToIrrlicht(uint32_t sdlKey, EKEY_CODE irrli
 	}
 }
 
-std::variant<u32, EKEY_CODE> CIrrDeviceSDL::getScancodeFromKey(const Keycode &key) const
+u32 CIrrDeviceSDL::getScancodeFromKey(const Keycode &key) const
 {
 	u32 keynum = 0;
 	if (const auto *keycode = std::get_if<EKEY_CODE>(&key)) {
-		// Fake keys (e.g. mouse buttons): use EKEY_CODE since there is no corresponding scancode.
-		if (is_fake_key(*keycode))
-			return *keycode;
 		// Try to convert the EKEY_CODE to a SDL scancode.
 		for (const auto &entry: KeyMap) {
 			if (entry.second == *keycode) {
@@ -328,14 +310,14 @@ std::variant<u32, EKEY_CODE> CIrrDeviceSDL::getScancodeFromKey(const Keycode &ke
 
 	// SDL3 returns a valid scancode for keycode 0. This is undesired.
 	if (keynum == 0)
-		return (u32) 0;
+		return 0;
 
 #ifdef _IRR_USE_SDL3_
 	SDL_Keymod kmod = SDL_KMOD_NONE; // TODO: respect modifiers
-	return (u32)SDL_GetScancodeFromKey(keynum, &kmod);
+	return SDL_GetScancodeFromKey(keynum, &kmod);
 #else
 	// Modifiers not supported
-	return (u32)SDL_GetScancodeFromKey(keynum);
+	return SDL_GetScancodeFromKey(keynum);
 #endif
 }
 
@@ -942,54 +924,35 @@ bool CIrrDeviceSDL::run()
 			if (button == SDL_BUTTON_X2)
 				button = SDL_BUTTON_RIGHT;
 #endif
+			irrevent.MouseInput.Button = button;
+			auto is_down = SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN;
+
+			if (is_down)
+				MouseButtonStates |= SDL_BUTTON_MASK(button);
+			else
+				MouseButtonStates &= ~SDL_BUTTON_MASK(button);
+
 			switch (button) {
 			case SDL_BUTTON_LEFT:
-				if (SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-					irrevent.MouseInput.Event = EMIE_LMOUSE_PRESSED_DOWN;
-					MouseButtonStates |= EMBSM_LEFT;
-				} else {
-					irrevent.MouseInput.Event = EMIE_LMOUSE_LEFT_UP;
-					MouseButtonStates &= ~EMBSM_LEFT;
-				}
+				irrevent.MouseInput.Event = is_down ? EMIE_LMOUSE_PRESSED_DOWN : EMIE_LMOUSE_LEFT_UP;
 				break;
 
 			case SDL_BUTTON_RIGHT:
-				if (SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-					irrevent.MouseInput.Event = EMIE_RMOUSE_PRESSED_DOWN;
-					MouseButtonStates |= EMBSM_RIGHT;
-				} else {
-					irrevent.MouseInput.Event = EMIE_RMOUSE_LEFT_UP;
-					MouseButtonStates &= ~EMBSM_RIGHT;
-				}
+				irrevent.MouseInput.Event = is_down ? EMIE_RMOUSE_PRESSED_DOWN : EMIE_RMOUSE_LEFT_UP;
 				break;
 
 			case SDL_BUTTON_MIDDLE:
-				if (SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-					irrevent.MouseInput.Event = EMIE_MMOUSE_PRESSED_DOWN;
-					MouseButtonStates |= EMBSM_MIDDLE;
-				} else {
-					irrevent.MouseInput.Event = EMIE_MMOUSE_LEFT_UP;
-					MouseButtonStates &= ~EMBSM_MIDDLE;
-				}
+				irrevent.MouseInput.Event = is_down ? EMIE_MMOUSE_PRESSED_DOWN : EMIE_MMOUSE_LEFT_UP;
 				break;
 
-			// Since Irrlicht does not have event types for X1/X2 buttons, we simply pass
-			// those as keycodes instead. This is relatively hacky but avoids the effort of
-			// adding more mouse events that will be discarded anyway once we switch to SDL
-			case SDL_BUTTON_X1:
-				irrevent.EventType = EET_KEY_INPUT_EVENT;
-				irrevent.KeyInput.Key = KEY_XBUTTON1;
-				break;
-
-			case SDL_BUTTON_X2:
-				irrevent.EventType = EET_KEY_INPUT_EVENT;
-				irrevent.KeyInput.Key = KEY_XBUTTON2;
+			default: // Other mouse buttons do not have dedicated events:
+				irrevent.MouseInput.Event = is_down ? EMIE_XMOUSE_PRESSED_DOWN : EMIE_XMOUSE_LEFT_UP;
 				break;
 			}
 
 			bool shift = (keymod & SDL_KMOD_SHIFT) != 0;
 			bool control = (keymod & SDL_KMOD_CTRL) != 0;
-			if (irrevent.EventType == EET_MOUSE_INPUT_EVENT && irrevent.MouseInput.Event != EMIE_MOUSE_MOVED) {
+			if (irrevent.MouseInput.Event != EMIE_MOUSE_MOVED) {
 				irrevent.MouseInput.ButtonStates = MouseButtonStates;
 				irrevent.MouseInput.X = static_cast<s32>(SDL_event.button.x * ScaleX);
 				irrevent.MouseInput.Y = static_cast<s32>(SDL_event.button.y * ScaleY);
@@ -1007,12 +970,6 @@ bool CIrrDeviceSDL::run()
 						postEventFromUser(irrevent);
 					}
 				}
-			} else if (irrevent.EventType == EET_KEY_INPUT_EVENT) {
-				irrevent.KeyInput.Char = 0;
-				irrevent.KeyInput.PressedDown = SDL_event.type == SDL_EVENT_MOUSE_BUTTON_DOWN;
-				irrevent.KeyInput.Shift = shift;
-				irrevent.KeyInput.Control = control;
-				postEventFromUser(irrevent);
 			}
 			break;
 		}

src/client/keycode.cpp
@@ -75,15 +75,8 @@ static std::vector<table_key> table = {
 
 	// Keys without a Char
 	// Note: we add "Key" to the description if the string could be confused for something else
-	DEFINEKEY1(KEY_LBUTTON, N_("Left Click"))
-	DEFINEKEY1(KEY_RBUTTON, N_("Right Click"))
 	// TRANSLATORS: Usually paired with the Pause key
 	DEFINEKEY1(KEY_CANCEL, N_("Break Key"))
-	DEFINEKEY1(KEY_MBUTTON, N_("Middle Click"))
-	// TRANSLATORS: Mouse button
-	DEFINEKEY1(KEY_XBUTTON1, N_("Mouse X1"))
-	// TRANSLATORS: Mouse button
-	DEFINEKEY1(KEY_XBUTTON2, N_("Mouse X2"))
 	DEFINEKEY1(KEY_BACK, N_("Backspace"))
 	DEFINEKEY1(KEY_TAB, N_("Tab Key"))
 	DEFINEKEY1(KEY_CLEAR, N_("Clear Key"))
@@ -273,36 +266,25 @@ static const table_key &lookup_keyname(std::string_view name)
 
 static const table_key &lookup_scancode(const u32 scancode)
 {
+	if (!scancode)
+		return invalid_key;
 	auto key = RenderingEngine::get_raw_device()->getKeyFromScancode(scancode);
 	return std::holds_alternative<EKEY_CODE>(key) ?
 		lookup_keykey(std::get<EKEY_CODE>(key)) :
 		lookup_keychar(std::get<wchar_t>(key));
 }
 
-const table_key &KeyPress::lookupScancode() const
-{
-	switch (getType()) {
-	case InputType::KEYBOARD:
-		return lookup_scancode(get<InputType::KEYBOARD>());
-	case InputType::LEGACY_KEYCODE:
-		return lookup_keykey(get<InputType::LEGACY_KEYCODE>());
-	default:
-		return invalid_key;
-	}
-}
-
 void KeyPress::loadFromKey(EKEY_CODE keycode, wchar_t keychar)
 {
-	auto irr_scancode = RenderingEngine::get_raw_device()->getScancodeFromKey(Keycode(keycode, keychar));
-	if (auto scancode = std::get_if<u32>(&irr_scancode))
-		emplace<InputType::KEYBOARD>(*scancode);
-	else
-		emplace<InputType::LEGACY_KEYCODE>(std::get<EKEY_CODE>(irr_scancode));
+	auto scancode = RenderingEngine::get_raw_device()->getScancodeFromKey(Keycode(keycode, keychar));
+	emplace<InputType::KEYBOARD>(scancode);
 }
 
 KeyPress::KeyPress(const std::string &name)
 {
-	if (loadFromScancode(name))
+	if (loadUnsignedFromPrefix<InputType::KEYBOARD>(name, "SYSTEM_SCANCODE_"))
+		return;
+	if (loadUnsignedFromPrefix<InputType::MOUSE_BUTTON>(name, "MOUSE_BUTTON_"))
 		return;
 	const auto &key = lookup_keyname(name);
 	loadFromKey(key.Key, key.Char);
@@ -313,7 +295,25 @@ KeyPress::KeyPress(const SEvent::SKeyInput &in)
 	if (in.SystemKeyCode)
 		emplace<InputType::KEYBOARD>(in.SystemKeyCode);
 	else
-		emplace<InputType::LEGACY_KEYCODE>(in.Key);
+		loadFromKey(in.Key, in.Char);
+}
+
+KeyPress::KeyPress(const SEvent::SMouseInput &in)
+{
+	switch (in.Event) {
+	case EMIE_LMOUSE_PRESSED_DOWN:
+	case EMIE_MMOUSE_PRESSED_DOWN:
+	case EMIE_RMOUSE_PRESSED_DOWN:
+	case EMIE_XMOUSE_PRESSED_DOWN:
+	case EMIE_LMOUSE_LEFT_UP:
+	case EMIE_MMOUSE_LEFT_UP:
+	case EMIE_RMOUSE_LEFT_UP:
+	case EMIE_XMOUSE_LEFT_UP:
+		emplace<InputType::MOUSE_BUTTON>(in.Button);
+		break;
+	default:
+		assert(false);
+	}
 }
 
 std::string KeyPress::sym() const
@@ -321,8 +321,8 @@ std::string KeyPress::sym() const
 	switch (getType()) {
 	case InputType::KEYBOARD:
 		return "SYSTEM_SCANCODE_" + std::to_string(get<InputType::KEYBOARD>());
-	case InputType::LEGACY_KEYCODE:
-		return lookupScancode().Name;
+	case InputType::MOUSE_BUTTON:
+		return "MOUSE_BUTTON_" + std::to_string(get<InputType::MOUSE_BUTTON>());
 	default:
 		return "";
 	}
@@ -331,29 +331,50 @@ std::string KeyPress::sym() const
 std::string KeyPress::name() const
 {
 	switch (getType()) {
-	case InputType::KEYBOARD:
-	case InputType::LEGACY_KEYCODE: {
-		const auto &name = lookupScancode().LangName;
+	case InputType::KEYBOARD: {
+		auto scancode = getScancode();
+		const auto &name = lookup_scancode(scancode).LangName;
 		if (!name.empty())
 			return strgettext(name);
-		if (auto scancode = getScancode())
+		if (scancode)
 			return fmtgettext("Scancode: %d", scancode);
 		return "";
+	}
+	case InputType::MOUSE_BUTTON: {
+		auto button = get<InputType::MOUSE_BUTTON>();
+		switch (button) {
+		case SDL_BUTTON_LEFT:
+			return strgettext("Left Click");
+		case SDL_BUTTON_MIDDLE:
+			return strgettext("Middle Click");
+		case SDL_BUTTON_RIGHT:
+			return strgettext("Right Click");
+		case SDL_BUTTON_X1:
+			// TRANSLATORS: This is a mouse button.
+			return strgettext("Mouse X1");
+		case SDL_BUTTON_X2:
+			// TRANSLATORS: This is a mouse button.
+			return strgettext("Mouse X2");
+		default:
+			// TRANSLATORS: This is for mouse buttons without an intuitive description. %d is the number of the button.
+			return fmtgettext("Mouse Button %d", button);
+		}
 	}
 	default:
 		return "";
 	}
 }
 
-bool KeyPress::loadFromScancode(const std::string &name)
+template<KeyPress::InputType I>
+bool KeyPress::loadUnsignedFromPrefix(const std::string &name, const std::string &prefix)
 {
-	if (!str_starts_with(name, "SYSTEM_SCANCODE_"))
+	if (!str_starts_with(name, prefix))
 		return false;
 	char *p;
-	const auto code = strtoul(name.c_str()+16, &p, 10);
+	const auto code = strtoul(name.c_str()+prefix.size(), &p, 10);
 	if (p != name.c_str() + name.size())
 		return false;
-	emplace<InputType::KEYBOARD>(code);
+	emplace<I>(code);
 	return true;
 }
 
@@ -362,8 +383,8 @@ KeyPress::operator bool() const
 	switch (getType()) {
 	case InputType::KEYBOARD:
 		return get<InputType::KEYBOARD>() != 0;
-	case InputType::LEGACY_KEYCODE:
-		return Keycode::isValid(get<InputType::LEGACY_KEYCODE>());
+	case InputType::MOUSE_BUTTON:
+		return get<InputType::MOUSE_BUTTON>() != 0;
 	case InputType::GAME_ACTION:
 		return get<InputType::GAME_ACTION>() < KeyType::INTERNAL_ENUM_COUNT;
 	default: