luanti-org/luanti

Use standard shading for item mesh rendering
Fixes #17110

src/client/mesh.cpp
@@ -186,13 +186,17 @@ static void applyToMesh(scene::IMesh *mesh, const F &fn)
 	}
 }
 
-void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color)
+void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color,
+		f32 ambient_light, v3f light_dir)
 {
-	applyToMeshBuffer(buf, [buf_color](auto *vertex) {
+	applyToMeshBuffer(buf, [=](auto *vertex) {
 		// Reset color
 		vertex->Color = buf_color;
 		// Apply shading
-		applyFacesShading(vertex->Color, vertex->Normal);
+		if (vertex->Normal == v3f())
+			return; // leave color unchanged ("fully lit")
+		const f32 intensity = std::fabs(vertex->Normal.normalize().dotProduct(-light_dir));
+		applyShadeFactor(vertex->Color, std::min(1.0f, ambient_light + intensity));
 	});
 }
 

src/client/mesh.h
@@ -57,9 +57,11 @@ void setMeshColor(scene::IMesh *mesh, video::SColor color);
 
 /*!
  * Overwrites the color of a mesh buffer.
- * The color is darkened based on the normal vector of the vertices.
+ * The color is darkened based on the normal vector of the vertices
+ * and the given directional light source + ambient light.
  */
-void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color);
+void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color,
+		f32 ambient_light, v3f light_dir);
 
 /*
 	Set the color of all vertices in the mesh.

billjamesevans/ai-native-luanti

src/client/mesh.cpp
@@ -186,13 +186,17 @@ static void applyToMesh(scene::IMesh *mesh, const F &fn)
 	}
 }
 
-void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color)
+void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color,
+		f32 ambient_light, v3f dir_light)
 {
-	applyToMeshBuffer(buf, [buf_color](auto *vertex) {
+	applyToMeshBuffer(buf, [=](auto *vertex) {
 		// Reset color
 		vertex->Color = buf_color;
 		// Apply shading
-		applyFacesShading(vertex->Color, vertex->Normal);
+		if (vertex->Normal == v3f())
+			return; // leave color unchanged ("fully lit")
+		const f32 intensity = std::fabs(vertex->Normal.normalize().dotProduct(-dir_light));
+		applyShadeFactor(vertex->Color, std::min(1.0f, ambient_light + intensity));
 	});
 }
 

src/client/mesh.h
@@ -57,9 +57,11 @@ void setMeshColor(scene::IMesh *mesh, video::SColor color);
 
 /*!
  * Overwrites the color of a mesh buffer.
- * The color is darkened based on the normal vector of the vertices.
+ * The color is darkened based on the normal vector of the vertices
+ * and the given directional light source + ambient light.
  */
-void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color);
+void colorizeMeshBuffer(scene::IMeshBuffer *buf, video::SColor buf_color,
+		f32 ambient_light, v3f dir_light);
 
 /*
 	Set the color of all vertices in the mesh.